876eceb320fc3e67f21144d381126d43e553fdaf
Leila Peña <leilajulieta1999@gmail.com>
2022-10-26 02:15:49 -0300

    agregue parametros a funcion fetchComics

--- fetchs/fetchComics.js
+++ fetchs/fetchComics.js
@@ -1,8 +1,8 @@
 const apiPublic = "ad190a0fc63213c3f3f9c4f4e562d713";
 const baseUrl = "https://gateway.marvel.com/v1/public/";
 
-const fetchComics = async () =>{
-    const response = await fetch(`${baseUrl}/comics?apikey=${apiPublic}&offset=0&orderBy=title`)
+const fetchComics = async (offset, orderBy) =>{
+    const response = await fetch(`${baseUrl}/comics?apikey=${apiPublic}&offset=${offset}&orderBy=${orderBy}`)
     const data = await response.json()
     return data
 }
