209bf6de9a42a52c3f3727ecdb6f851027651a01
Mayaba93 <baezmagali@live.com>
2022-10-25 21:27:20 -0300

    Creamos cards

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
+
+
 const loadComics = async () =>{
     const comicsResponse = await fetchComics();
     const data = comicsResponse.data;
     const comics = data.results;
-}
+    const containerCards = document.getElementById('containerCards');
+    comics.forEach(comic => {
+        const comicCard = document.createElement("div");
+        const cardImg = document.createElement("img");
+        const imgContainer = document.createElement("div");
+        const cardTitle = document.createElement("h3");
+        const cardTitleText= document.createTextNode(comic.title)
+
+        comicCard.appendChild(imgContainer);
+        containerCards.appendChild(comicCard);
+        imgContainer.appendChild(cardImg);
+        comicCard.appendChild(cardTitle);
+        cardTitle.appendChild(cardTitleText);
+        
+
+
+
+        comicCard.classList.add("comicCard");
+        imgContainer.classList.add("imgContainer");
+        cardImg.classList.add("cardImg");
+        cardTitle.classList.add("comicTitle")
+
+        cardImg.setAttribute("src" , `${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`)
+    });
+
+};
 loadComics()
+
+
